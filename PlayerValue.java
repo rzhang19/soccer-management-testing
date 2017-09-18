bc75e625753dfb7104c91ea4760700e55a915f10
Robin Zhang <rzhang19@users.noreply.github.com>
2017-09-18 10:38:00 -0700

    Add files via upload

---
+++ PlayerValue.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
+public class PlayerValue {
+   public static void main (String[] args) {
+      /*
+      So far, given what we have in the Player.cpp and Player.h files,
+      the following affect the base player value:
+      1) Rating      (exponential)
+      2) Age         (bell curve, peak when reached potential)
+      3) Potential   (exponential)
+      
+      For young players (age<23), potential is weighed more heavily than overall.
+      While players can grow even after they pass age 23, potential affects their
+      value less and is not completely ignored.
+      
+      The same player may also be valued differently by different clubs.
+      */
+      
+      
+   }
+}
